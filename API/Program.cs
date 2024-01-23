538b28432b33476e1283478dcde19504598aaeda
Roberto Assumpção <betoassumpcao@outlook.com>
2024-01-23 17:12:43 -0300

    prática 1

--- API/Program.cs
+++ API/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HttpClient client = new();
+string? resposa = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+Console.WriteLine(resposa);
---
+++ cheapshark/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    using HttpClient client = new();
+    string? resposa = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
+    Console.WriteLine(resposa);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
